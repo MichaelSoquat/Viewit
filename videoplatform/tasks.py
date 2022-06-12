c0a5153068e9348e0f497877ff6800ecc5f1d209
MichaelSoquat <michael.soquat@gmx.de>
2022-06-12 09:49:51 +0200

    added converting into 480p

--- videoplatform/tasks.py
+++ videoplatform/tasks.py
@@ -1,3 +1,7 @@
+import subprocess
+
 def convert_480p(source):
-    print(source)             
-    # Befehl hinzufügen
+    target = source + '_480p.mp4'
+    cmd = 'ffmpeg -i "{0}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{1}"'.format(source, target)
+    subprocess.run(cmd)              
+   
